9cd4a0c3aa7b4bd852edba7e207936347a9b5c06
domreader <33025225+domreader@users.noreply.github.com>
2020-03-02 13:13:32 +0000

    Productive Session (02/03)
    First session of the month of March has gone well. In regards to progress there is now a game loop implemented as well as a rudimentary objective system. I am happy with how the early stages of prototyping are going and screenshots to follow

---
+++ Learn To Cook VR/Assets/Scripts/Cooking.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Cooking : MonoBehaviour
+{
+
+    bool isButterAdded = false;
+    bool isFlourAdded = false;
+
+    [SerializeField]
+    GameObject Flour;
+    [SerializeField]
+    GameObject Butter;
+
+    public Sprite[] sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Image").GetComponent<Image>().sprite = sprites[0];
+
+        Debug.Log("You even listening");
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.gameObject.tag == "Flour")
+        {
+            GameObject.Find("Image").GetComponent<Image>().sprite = sprites[1];
+            Debug.Log("Flour Added");
+            isFlourAdded = true;
+            Destroy(Flour);
+
+        }
+
+        if (collider.gameObject.tag == "Butter")
+        {
+            GameObject.Find("Image").GetComponent<Image>().sprite = sprites[2];
+            Debug.Log("Butter Added");
+            isButterAdded = true;
+            Destroy(Butter);
+
+        }
+
+        if (isButterAdded == true && isFlourAdded == true)
+        {
+            Debug.Log("Congratulations ... You Can Drop Stuff In A Pan");
+
+        }
+
+        if(collider.gameObject.tag == "placeMat")
+        {
+            SceneManager.LoadScene("Menu Screen");
+        }
+
+
+    }
+}
