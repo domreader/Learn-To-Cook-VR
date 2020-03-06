8cf1187194b215530ebc2b8253b83c3386bb787d
domreader <dominic.reader@students.plymouth.ac.uk>
2020-03-06 23:40:40 +0000

    Major Progress (06/03)
    Throughout the day I have been working on many different elements of LTC VR. Please see change log below:
    
    - Updated game loop to feature cooking (simulated) as well as ability to turn sauce into other items.
    
    - Menu room now has artwork which is consistent across the whole game (this was made with poster software Canva)
    
    - Area lights have now been added and baking is not taking an age ... result!
    
    - Starting to work on another recipe room to add more content in.
    
    Overall with the progress made today has been very successful and moving forwards adding these recipes and updating art, scripts etc. will be key

---
+++ Learn To Cook VR/Assets/Scripts/MakingRoux.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MakingRoux : MonoBehaviour
+{
+
+    bool isButterAdded = false;
+    bool isFlourAdded = false;
+    bool isStockAdded = false;
+    bool isMilkAdded = false;
+    bool allIngredientsPresent = false;
+    bool isEverythingCorrect = false;
+    float timer;
+
+    [SerializeField]
+    GameObject Flour;
+    [SerializeField]
+    GameObject Butter;
+    [SerializeField]
+    GameObject Milk;
+    [SerializeField]
+    GameObject Stock;
+
+    public Sprite[] sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Image").GetComponent<Image>().sprite = sprites[0];
+
+        Debug.Log("You even listening");
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+
+        if (collider.gameObject.tag == "Butter")
+        {
+
+            GameObject.Find("Image").GetComponent<Image>().sprite = sprites[1];
+            Debug.Log("Butter Added");
+            isFlourAdded = true;
+            Destroy(Butter);
+
+        }
+
+        if (collider.gameObject.tag == "Flour")
+        {
+
+            GameObject.Find("Image").GetComponent<Image>().sprite = sprites[2];
+            Debug.Log("Flour Added");
+            isButterAdded = true;
+            Destroy(Flour);
+
+        }
+
+        if (isButterAdded == true && isFlourAdded == true)
+        {
+
+            Debug.Log("Congratulations ... You Can Drop Stuff In A Pan");
+            Debug.Log("Now Put The Pan On The Heat Till Flour Is Cooked Out");
+
+            allIngredientsPresent = true;
+
+        }
+
+        if (collider.gameObject.tag == "Hob" && allIngredientsPresent == true)
+        {
+
+           Debug.Log("Cooking time is " + (timer + 1) + "Seconds" );
+          
+            timer += Time.deltaTime;
+
+           if (timer >= 5)
+            {
+
+                Debug.Log("Take the pan off heat");
+                isEverythingCorrect = true;
+                GameObject.Find("Image").GetComponent<Image>().sprite = sprites[3];
+               
+            }
+
+            if (timer >= 10)
+            {
+                Debug.Log("Try Again");
+                GameObject.Find("Image").GetComponent<Image>().sprite = sprites[4];
+                isEverythingCorrect = false;
+            }
+
+        }
+
+        if (isEverythingCorrect == true && collider.gameObject.tag == "Milk")
+        {
+            Debug.Log("That's Now A Bechemel Sauce ... Go You");
+            isMilkAdded = true;
+            Destroy(Milk);
+        }
+
+        if (isEverythingCorrect == true && collider.gameObject.tag == "Stock")
+        {
+            Debug.Log("That's Now A Veloute Sauce ... Look At You Being All Fancy");
+            isStockAdded = true;
+            Destroy(Stock);
+        }
+
+        if (isEverythingCorrect == true && collider.gameObject.tag == "placeMat")
+        {
+        
+            SceneManager.LoadScene("Menu Screen");
+
+        }
+
+        if (isEverythingCorrect == false && collider.gameObject.tag == "placeMat")
+        {
+
+            Debug.Log("Hmmm ... That is not correct");
+
+        }
+    }
+}
--- Learn To Cook VR/Assets/Scripts/Scenes.cs
+++ Learn To Cook VR/Assets/Scripts/Scenes.cs
@@ -2,9 +2,30 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 public class Scenes : MonoBehaviour {
- public void StartGame()
-{
- SceneManager.LoadScene( "Kitchen");
-}
+    public void GoToRoux()
+    {
+        SceneManager.LoadScene("Roux");
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu Screen");
+    }
+
+    public void BurgersAndDogs()
+    {
+        SceneManager.LoadScene("Burgers & Dogs");
+    }
+
+    public void LoseRoom()
+    {
+        SceneManager.LoadScene("Lose Room");
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+        Debug.Log("Quitting Game");
+    }
 
 }
