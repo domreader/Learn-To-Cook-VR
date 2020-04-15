01802570034418f6f5d9a1069f9e9ef6fd3c9bf6
domreader <dominic.reader@students.plymouth.ac.uk>
2020-04-15 13:06:36 +0100

    Revert "Revert "Functions Working""
    This reverts commit dec17792ca794fbdd54084a782db91073942b483.

---
+++ Learn To Cook VR/Assets/Scripts/ColourHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourHandler : MonoBehaviour
+{
+    public GameObject colouredEdge1;
+    public GameObject colouredEdge2;
+    public GameObject colouredEdge3;
+    public GameObject buttonEdge;
+    
+
+    void Start()
+    {
+        
+
+
+
+    }
+    
+}
--- Learn To Cook VR/Assets/Scripts/itemList.cs
+++ Learn To Cook VR/Assets/Scripts/itemList.cs
@@ -6,11 +6,13 @@ public class itemList : MonoBehaviour
 {
 
     public List<string> currentItems;
+    public List<GameObject> storedContacts;
 
     // Start is called before the first frame update
     void Start()
     {
         currentItems = new List<string>();
+        storedContacts = new List<GameObject>();
     }
 
 
